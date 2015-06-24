c75ec99efbbc64cc5b0d4aaab9f0f7569e479b29
NinjaEngineer <NinjaEngineer@users.noreply.github.com>
2015-06-24 15:41:25 -0400

    changed variable names and started on functions

--- stock-analytics.py
+++ stock-analytics.py
@@ -1,28 +1,34 @@
+#Written for the Florida Polytechnic Big Data Analytics Lab
+#By Jean-Patrice Jean-Louis
+
 from yahoo_finance import Share
-from pprint import pprint
 from csv import DictWriter
 import json
 
-stock = input("Please enter a stock symbool to get info: ")
+symbol = input("Please enter a symbol symbol to get info: ")
+startDate = input("Enter analytics start date (YYYY-MM-DD): ")
+endDate = input("Enter analytics end date (YYYY-MM-DD): ")
+csvFile = input("Enter a name for the saved .csv file, omitting the .csv ") + ".csv"
 
-stock = Share(stock)
-a = input("Enter analytics start date (YYYY-MM-DD): ")
-b = input("Enter analytics end date (YYYY-MM-DD): ")
-print("Current price: ",stock.get_price())
-data = stock.get_historical(a, b)
-pprint(data)
-
+# def getstockjson():
+symbol = Share(symbol)
+data = symbol.get_historical(startDate, endDate)
 json_str = json.dumps(data)
 with open('data.json', 'w') as f:
-     json.dump(data, f)
-
+    json.dump(data, f)
 dicts = json.loads(json_str)
-name = input("Enter a name for the saved .csv file: ")
-savedfile = open(name+".csv", "w")
-writer = DictWriter(savedfile, dicts[0].keys())
+
+
+saveFile = open(csvFile, "w")
+writer = DictWriter(saveFile, dicts[0].keys(), lineterminator='\n')
 writer.writeheader()
 writer.writerows(dicts)
-savedfile.close()
+saveFile.close()
 
-#Written for the Florida Polytechnic Big Data Analytics Lab
-#By Jean-Patrice Jean-Louis
+#
+# def main():
+#     getstockjson()
+#     saveStockCsv()
+#
+# if __name__ == "__main__":
+#     main()
